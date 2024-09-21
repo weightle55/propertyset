1c929bfd886a5ae6db8211e73b2b8c983b475738
weightle55 <metalicboots@gmail.com>
2024-09-21 14:22:36 +0900

    first commit

---
+++ src/main/java/com/example/propertyset/PropertysetApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertyset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertysetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PropertysetApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/propertyset/PropertysetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertyset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertysetApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
